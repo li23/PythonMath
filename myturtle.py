8763ee5db1fe00c01819dc239e4dbda87664d463
T.Chisholm <chisholntw@yahoo.com>
2019-11-18 14:01:02 -0800

    turtle stuff

---
+++ myturtle.py
@@ -0,0 +1,9 @@
+#This one works
+import turtle
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+# etc.
+
+turtle.getscreen()._root.mainloop()  # <-- run the Tkinter main loop
---
+++ myturtle2.py
@@ -0,0 +1,7 @@
+#This one works
+from turtle import *
+shape('turtle')
+for i in range(4):
+    forward(100)
+    right(90)
+#turtle.getscreen()._root.mainloop()  # <-- run the Tkinter main loop
