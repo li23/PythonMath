99007ae64d966cc5b7d63d75276774a6f8e1f444
T.Chisholm <chisholntw@yahoo.com>
2019-11-19 07:18:06 -0800

    new files

---
+++ conditionals.py
@@ -0,0 +1,18 @@
+y = 6
+if y > 7:
+    print("yes!")
+else:
+    print("no!")
+
+
+age = 50
+if age < 10:
+    print("What School do you go to?")
+elif 11 < age < 20:
+    print("Yr cool!")
+elif 20 <  age < 30:
+    print("What job do you have?")
+elif 30 < age < 40:
+    print("Are u married?")
+else:
+    print("Fuck ur OLD!")
---
+++ factors.py
@@ -0,0 +1,8 @@
+#factors.py
+def factors(num):
+    '''Returns a list of the factors of num'''
+    factorList = []
+    for i in range(1,num+1):
+        if num % i == 0:
+            factorList.append(i)
+    return factorList
